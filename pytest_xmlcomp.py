03304276b73e31de0c1605cdf85231a134aa3502
Fabian Baumanis <fabian.baumanis@suse.com>
2018-04-26 13:55:06 +0200

    Add fixture for checking xml, out and err files.
    Signed-off-by: Fabian Baumanis <fabian.baumanis@suse.com>

--- pytest_xmlcomp.py
+++ pytest_xmlcomp.py
@@ -3,21 +3,42 @@
 __version__ = "0.2.0"
 
 import pytest
-
+from py.path import local
 
 def pytest_addoption(parser):
     group = parser.getgroup('xmlcomp')
     group.addoption(
-        '--foo',
+        '--datadir',
         action='store',
-        dest='dest_foo',
-        default='2018',
-        help='Set the value for the fixture "bar".'
-    )
+        dest='datadir',
+        default='tests/data',
+        help='Give the directory containing the XML files.')
 
     parser.addini('HELLO', 'Dummy pytest.ini setting')
 
 
 @pytest.fixture
 def bar(request):
-    return request.config.option.dest_foo
+    return request.config.option.datadir
+
+@pytest.fixture
+def check_for_files(request):
+    result = []
+    # create Pathlib object for the given data directory
+    p = local(request.config.option.datadir)
+    # create filter for listdir function
+    def xml_filter(x):
+        if x.ext == ".xml":
+            return x
+    #iterate over the file list
+    for f in p.listdir(fil=xml_filter):
+        out = f.new(ext = ".out")
+        if not out.exists():
+            print("No output files found")
+            continue
+        err = f.new(ext = ".err")
+        if not err.exists():
+            result.append((f,out,None))
+        else:
+            result.append((f,out,err))
+    return result
